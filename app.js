4975998571cbfa8c13ee01db09c83e05dddd9138
GermanHv <ghove7@gmail.com>
2022-04-27 18:07:08 -0500

    Agregando endopoint POST

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express')
 const app = express()
 app.use(express.json()) // Indicamos que usaremos JSON
 // Puerto en que vamos a ver nuestra app: localhost:3000
-const port = 3000
+const port = 3001
+// ENDPOINT
 //routes
 //Methods HTTP
 app.get('/v1/explorers', (req,res) =>{
@@ -16,7 +17,19 @@ app.get('/v1/explorers', (req,res) =>{
     res.status(200).json(explorers)
 })
 
+app.get('/v1/explorers/:id', (req,res) =>{
+    console.log (`GET Explorers V1 API ${new Date()}`)
+    const explorer1 = {id:1, name:"Explorer1"}
+    // HTTP CODE STATUS: 200
+    res.status(200).json(explorer1)
+})
 
+app.post('/v1/explorers', (req,res) =>{
+    console.log (`POST Explorers V1 API ${new Date()}`)
+   //Agregar la logica para persistir
+   console.log(req.body) //parametros del request
+   res.status(201).json({message: "Creando exitosamente"})
+})
 
 // Con esto inicializamos esta app
 app.listen(port, () => {
